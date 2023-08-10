88c760c5d5a7f06c7b1d141522ae2fd7febc1ade
sinyoung <kay9508@naver.com>
2023-08-11 05:29:31 +0900

    README & 주석 추가

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # batch_template
 SpringBoot V-2.x와 V-3.x에서의 batch작성 예시
 
-관련내용 벨로그에 기재 예정
+• Spring batch의 메타 테이블 구조
+
+![domain1](./images/domain1.png)
+
+( 출처: [metaDataSchema](https://docs.spring.io/spring-batch/3.0.x/reference/html/metaDataSchema.html) )
+관련 sql문 위치 : spring-batch-core-(version).jar 안에 위치해 있다. (schema-라는 파일명을 검색하면 쉽게 찾을 수 있다.)
+
+이 테이블들이 있어야만 Spring Batch가 정상 작동한다.
+
--- src/main/java/com/example/batch_template/batch/SpringBoot_Version3X/ManyStepBatch_Version3X.java
+++ src/main/java/com/example/batch_template/batch/SpringBoot_Version3X/ManyStepBatch_Version3X.java
@@ -1,9 +1,11 @@
 package com.example.batch_template.batch.SpringBoot_Version3X;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
@@ -12,7 +14,8 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@RequiredArgsConstructor
+@Slf4j // log 사용을 위한 lombok 어노테이션
+@RequiredArgsConstructor // 생성자 DI를 위한 lombok 어노테이션
 @Configuration
 public class ManyStepBatch_Version3X extends DefaultBatchConfiguration {
     // 다른 @Service 비즈니스 로직도 DI로 사용 가능
@@ -24,29 +27,41 @@ public class ManyStepBatch_Version3X extends DefaultBatchConfiguration {
         return new JobBuilder("testJob", jobRepository)
                 // testStep1 시작
                 .start(testStep1())
+
                 // testStep1 수행 결과 ExitStatus.FAILED 일 경우
                 .on("FAILED")
+
                 // failStep1 수행
                 .to(failStep1())
+
                 // failStep1 수행 결과에 상관 없이
                 .on("*")
+
                 // testStep1 종료
                 .end()
+
                 // testStep1 후에
                 .from(testStep1())
+
                 // testStep1 수행 결과에 상관 없이
                 .on("*")
+
                 // tesetStep2로 다음 Step 수행
                 .to(testStep2())
+
                 // testStep2 수행 결과에 상관 없이
                 // 다음 수행으로 testStep3 지정
                 .next(testStep3())
+
                 // testStep3 수행 결과에 상관없이
                 .on("*")
+
                 // stepFlow 종료(FlowBuilder 반환)
                 .end()
+
                 // 해당 Job 종료(FlowBuilder 종료)
                 .end()
+
                 .build();
     }
 
@@ -56,6 +71,7 @@ public class ManyStepBatch_Version3X extends DefaultBatchConfiguration {
     @Bean
     public Step testStep1() {
         return new StepBuilder("testStep1", jobRepository)
+                // Tasklet은 Step안에서 단일로 수행될 커스텀한 기능들을 선언할때 사용
                 .tasklet((contribution, chunkContext) ->{
 
                     // Batch 로직 구현
@@ -70,6 +86,30 @@ public class ManyStepBatch_Version3X extends DefaultBatchConfiguration {
                 })
                 .build();
     }
+
+    /*
+    (관련 내용은 BATCH_JOB_INSTANCE 테이블에서 확인할 수 있다.)
+
+    @Bean
+    @JobScope // Job Parameter로 받은 값을 로그에 추가로 출력시키는 기능 (같은파라미터 일경우 Job이 한번 더 실행되는 것을 막을 때 사용한다.)
+    public Step testStep1(@Value("#{jobParameters[requestDate]}") {
+        return new StepBuilder("testStep1", jobRepository)
+                // Tasklet은 Step안에서 단일로 수행될 커스텀한 기능들을 선언할때 사용
+                .tasklet((contribution, chunkContext) ->{
+
+                    // Batch 로직 구현
+                    //...
+
+                    // 아래 구문으로 로직 도중 FAILED 처리 가능
+                    // ExitStatus 안의 값에 따라 분기 가능
+                    contribution.setExitStatus(ExitStatus.FAILED);
+
+                    // 정상 종료 시 상태 Return
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+    */
 
     @Bean
     public Step failStep1() {
